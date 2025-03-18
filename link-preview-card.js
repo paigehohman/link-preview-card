878edcab7fbf84b96e8156605f10ebca693b28dc
paigehohman <pjh5761@psu.edu>
2025-03-18 15:41:59 -0400

    commit

--- link-preview-card.js
+++ link-preview-card.js
@@ -20,12 +20,12 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
-    this.url = "https://hax.psu.edu";
-    this.t = this.t || {};
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
+    this.description = "";
+    this.href = "";
+    this.image = "";
+    this.link = "";
+    this.loading = false;
+
     this.registerLocalization({
       context: this,
       localesPath:
@@ -37,12 +37,13 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
 
   updated(changedProperties) {
     super.updated(changedProperties);
-    if (changedProperties.has("url")) {
-      this.date = this.getData(this.url);
+    if (changedProperties.has("href")) {
+      this.date = this.getData(this.href);
     }
   }
 
   async getData(link) {
+    this.loading = true;
     const url = `https://open-apis.hax.cloud/api/services/website/metadata?q=${link}`;
     try {
       const response = await fetch(url);
@@ -58,11 +59,25 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
       //   2
       // );
       // document.querySelector("#there").innerHTML = json.data["og:site_name"];
-      this.title = json.data.title;
-      console.log(json.data.description);
-      console.log(json.data.url);
+      this.title =
+        json.data["og:title"] || json.data["title"] || "Not Available";
+      this.description = json.data["description"] || "Not Available";
+      this.image = this.image || "/assets/default-preview.png";
+      //  json.data["image"] ||
+      //  json.data["logo"] ||
+      //  json.data["og:image"] ||
+      // json.data["twitter:image"] ||
+      //  json.data["thumbnail"] ||
+      //  "";
+      this.link = json.data["url"] || link;
     } catch (error) {
-      console.error(error.message);
+      console.error("Error", error);
+      this.title = "No Preview";
+      this.description = "";
+      this.image = "";
+      this.link = "";
+    } finally {
+      this.loading = false;
     }
   }
 
@@ -83,7 +98,11 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
       ...super.properties,
       title: { type: String },
       data: { type: Object },
-      url: { type: String },
+      description: { type: String },
+      href: { type: String },
+      image: { type: String },
+      link: { type: String },
+      loading: { type: Boolean, reflect: true },
     };
   }
 
@@ -97,10 +116,37 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
           color: var(--ddd-theme-primary);
           background-color: var(--ddd-theme-accent);
           font-family: var(--ddd-font-navigation);
+          padding: 10px;
+          max-width: 400px;
+          border-radius: 10px;
+          border: 1px solid;
         }
         .wrapper {
           margin: var(--ddd-spacing-2);
           padding: var(--ddd-spacing-4);
+        }
+        img {
+          max-width: 100%;
+          border-radius: 10px;
+        }
+        .title {
+          font-weight: bold;
+          color: var(--ddd-primary-2);
+        }
+        .loading-spinner {
+          margin: 30px auto;
+          border: 8px solid #323ca8;
+          width: 20px;
+          height: 20px;
+          animation: spin 3s linear infinite;
+        }
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
         }
         h3 span {
           font-size: var(
@@ -114,11 +160,24 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
 
   // Lit render the HTML
   render() {
-    return html` <div class="wrapper">
-      <div>${this.title}</div>
-      <div></div>
-      <div></div>
-    </div>`;
+    return html`
+      <div class="wrapper">
+        ${this.loading
+          ? html` <div class="loading-spinner"></div> `
+          : html`
+              ${this.image
+                ? html` <img src="${this.image}" alt="Preview Image" />`
+                : ""}
+              <div class="content">
+                <h3 class="title">${this.title}</h3>
+                <p class="desc">${this.description}</p>
+                <a href="${this.link}" target="_blank" class="url"
+                  >Visit Site</a
+                >
+              </div>
+            `}
+      </div>
+    `;
   }
 
   /**
